f1acda62dd259bf2e8e41c007c5711dc5a870cd2
banbanmumani <github.dinky@gmail.com>
2018-02-16 17:14:34 +0900

    요구사항1 구현
    index.html 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,11 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -24,10 +22,45 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            int i = 0;
+            StringBuffer sb = new StringBuffer();
+
+            log.debug("read request...");
+            String line = null;
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            do {
+                line = reader.readLine();
+                sb.append(line).append("\r\n");
+                if (line == null)
+                    break;
+            } while (!"".equals(line));
+
+            log.debug("read request finish");
+
+            String request = sb.toString();
+            String[] reqArr = request.split("\r\n");
+
+            String[] header = reqArr[0].split(" ");
+
+            String method = header[0];
+            String path = header[1];
+            String protocol = header[2];
+
+            byte[] body = null;
+
+            if ("/index.html".equals(path)) {
+                File file = new File("./webapp/index.html");
+                Scanner scanner = new Scanner(file);
+                StringBuffer html = new StringBuffer();
+                while (scanner.hasNext()) {
+                    html.append(scanner.nextLine());
+                }
+                body = html.toString().getBytes();
+                DataOutputStream dos = new DataOutputStream(out);
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            }
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
